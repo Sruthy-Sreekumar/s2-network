0127122b167337568920aca5f54888e8651dff93
Sruthy-Sreekumar <148990178+Sruthy-Sreekumar@users.noreply.github.com>
2024-03-31 18:53:43 +0530

    strings are equal or not

---
+++ string.sh
@@ -0,0 +1,12 @@
+echo "Enter the first string:"
+read a
+echo "Enter the second string:"
+read b
+if [ "$a" == "$b" ]
+then
+ echo "Both strings are equal"
+else
+ echo "Strings are not equal"
+fi
+
+
